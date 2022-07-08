aed67a296b419d5f5722c3c88f211e4232c55996
Steph Karlsen <becomingsteph@gmail.com>
2022-07-08 17:29:12 -0600

    Initial Commit

---
+++ src/com/skilldistillery/blackjack/entities/Deck.java
@@ -0,0 +1,41 @@
+package com.skilldistillery.blackjack.entities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	private List<Card> cards;
+
+	public Deck() {
+		cards = createDeck();
+	}
+
+	public List<Card> createDeck() {
+		List<Card> deck = new ArrayList<>(52);
+		
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				deck.add(new Card(suit, rank));
+
+			}
+
+		}
+		return deck;
+	}
+
+//	public int checkDeckSize() {
+//		return deck.size();
+//
+//	}
+
+	public Card dealCard() {
+		Card card = cards.remove(0);
+		return card;
+	}
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+
+	}
+}
---
+++ src/com/skilldistillery/blackjack/entities/Suit.java
@@ -0,0 +1,25 @@
+package com.skilldistillery.blackjack.entities;
+
+public enum Suit {
+
+	HEARTS("Hearts"), SPADES("Spades"), CLUBS("Clubs"), DIAMONDS("Diamonds");
+
+	private String name;
+
+	Suit() {
+	}
+
+	Suit(String suit) {
+		this.name = suit;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
